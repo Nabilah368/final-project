0ae61f5ffea625b886ebd76d328a37b133b46958
Nabilah368 <nabilahnabill@gmail.com>
2025-06-05 22:07:04 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -19,9 +19,7 @@ class TranslationService:
             base64_image = base64.b64encode(image_bytes).decode('utf-8')
             
             headers = {
-                "Authorization": f"Bearer {api_key}",
-                "HTTP-Referer": "https://final-project-text-image-translate.streamlit.app",  # atau domain kamu saat deploy
-                "X-Title": "AI Document Translator",
+                "Authorization": f"Bearer {self.translator_key}",
                 "Content-Type": "application/json"
             }
             
@@ -98,9 +96,9 @@ class HistoryManager:
 class TranslatorApp:
     def __init__(self):
         self.translator = TranslationService(
-            api_key="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",
-            model="qwen/qwen2.5-vl-32b-instruct",  # Model yang mendukung analisis gambar
-            api_url="https://openrouter.ai/api/v1"
+            api_key="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",
+            model="meta-llama/llama-4-maverick",  # Model yang mendukung analisis gambar
+            api_url="https://openrouter.ai/api/v1/chat/completions"
         )
         self.history_manager = HistoryManager()
 
